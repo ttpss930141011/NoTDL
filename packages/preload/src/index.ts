bee984aad250cb0572509d9ffa6480674a1c3f60
Justin Xiao <o927416847@gmail.com>
2024-07-10 20:38:04 -0700

    :sparkles: Enable auth function in front end
    Signed-off-by: Justin Xiao <o927416847@gmail.com>

--- packages/preload/src/index.ts
+++ packages/preload/src/index.ts
@@ -6,5 +6,6 @@ import {sha256sum} from './nodeCrypto';
 import {versions} from './versions';
 import tasks from './tasks';
 import agent from './agent';
+import user from './user';
 
-export {sha256sum, versions, tasks, agent};
+export {sha256sum, versions, tasks, agent, user};
